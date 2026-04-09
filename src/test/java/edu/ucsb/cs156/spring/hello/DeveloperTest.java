bbd308084e60a3c9c0dfa412e43ab52732a3efa8
Katie Pyo <pyokatie@gmail.com>
2026-04-08 17:30:53 -0700

    kp - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Soome", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("katskt", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
