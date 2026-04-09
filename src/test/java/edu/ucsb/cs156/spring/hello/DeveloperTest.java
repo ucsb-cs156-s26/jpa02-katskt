5c64f9e17869119d29dfef9e742c3c8f2e4ac951
Katie Pyo <pyokatie@gmail.com>
2026-04-08 17:39:40 -0700

    kp - updated with team members

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -30,6 +30,24 @@ public class DeveloperTest {
         assertEquals("katskt", Developer.getGithubId());
     }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team t = Developer.getTeam();
+        assertEquals("s26-05", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Ibrahim"), "Team should contain Ibrahim");
+        assertTrue(t.getMembers().contains("Alexia"), "Team should contain Alexia");
+        assertTrue(t.getMembers().contains("Elissa"), "Team should contain Elissa");
+        assertTrue(t.getMembers().contains("John"), "Team should contain John");
+        assertTrue(t.getMembers().contains("Joaquin"), "Team should contain Joaquin");
+        assertTrue(t.getMembers().contains("Soome"), "Team should contain Soome");
+
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
